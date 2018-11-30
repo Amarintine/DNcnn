87f41401c0fe53a3535a2b78a2e67f4694c02ed2
Amarintine <cx19960812@gmail.com>
2018-11-30 10:01:44 +0800

    Add files via upload

--- data_loader.py
+++ data_loader.py
@@ -1,13 +1,12 @@
-# encoding: utf-8
 from utils import *
 import torch
 from PIL import Image
 import numpy as np
 import glob
 import os
-scales = [0.5]  #1,0.9,0.8,0.7
-STRIDE = 10
-PAT_SIZE = 40
+scales = [1,0.5]  #1,0.9,0.8,0.7
+STRIDE = 16
+PAT_SIZE = 64
 BAT_SIZE = 64
 class Data_loader():
 
@@ -16,48 +15,57 @@ class Data_loader():
 
     def load_data(self,args):
 
-        files1, files2 = os.listdir('.//datasets//train/input'), os.listdir('./datasets/train/labels')
+        files1, files2 = sorted(os.listdir('.//datasets//train/inputs')), sorted(os.listdir('./datasets/train/labels'))
         data = []
         for filepath1 in files1:
-            filepath1 = glob.glob('./datasets/train/input/' + filepath1 + '/*.tif')
+            filepath1 = sorted(glob.glob('./datasets/train/inputs/' + filepath1 + '/*.tif'))
             data.append(self.generate_3D_patches(filepath1))
         for filepath2 in files2:
-            filepath2 = glob.glob('./datasets/train/labels/' + filepath2 + '/*.tif')
+            filepath2 = sorted(glob.glob('./datasets/train/labels/' + filepath2 + '/*.tif'))
             data.append(self.generate_3D_patches(filepath2))
 
         # normalization [0,1]
-        self.data1 = data[0].astype(np.float32) / 65535.0  # [9856,1,30,40,40]  create——data batch=64
-        self.data2 = data[1].astype(np.float32) / 65535.0
-        self.data3 = data[2].astype(np.float32) / 65535.0
-        self.data4 = data[3].astype(np.float32) / 65535.0
-        self.data5 = data[4].astype(np.float32) / 65535.0
-        self.data6 = data[5].astype(np.float32) / 65535.0
+        self.data1 = data[0].astype(np.float32) / 255.0  # [9856,1,30,40,40]  create——data batch=64
+        self.data2 = data[1].astype(np.float32) / 255.0
+        # self.data3 = data[2].astype(np.float32) / 255.0
+        # self.data4 = data[3].astype(np.float32) / 255.0
+        # self.data5 = data[4].astype(np.float32) / 255.0
+        # self.data6 = data[5].astype(np.float32) / 255.0
+        # self.data7 = data[6].astype(np.float32) / 255.0
+        # self.data8 = data[7].astype(np.float32) / 255.0
+        # self.data9 = data[8].astype(np.float32) / 255.0
+        # self.data10= data[9].astype(np.float32) / 255.0
 
         print("random load datasets")
         randnum = np.random.randint(0, 100)
-        np.random.seed(randnum)
-        np.random.shuffle(self.data1)
-        np.random.seed(randnum)
-        np.random.shuffle(self.data2)
-        np.random.seed(randnum)
-        np.random.shuffle(self.data3)
-        np.random.seed(randnum)
-        np.random.shuffle(self.data4)
-        np.random.seed(randnum)
-        np.random.shuffle(self.data5)
-        np.random.seed(randnum)
-        np.random.shuffle(self.data6)
+        np.random.seed(randnum);np.random.shuffle(self.data1)
+        np.random.seed(randnum);np.random.shuffle(self.data2)
+        # np.random.seed(randnum);np.random.shuffle(self.data3)
+        # np.random.seed(randnum);np.random.shuffle(self.data4)
+        # np.random.seed(randnum);np.random.shuffle(self.data5)
+        # np.random.seed(randnum);np.random.shuffle(self.data6)
+        # np.random.seed(randnum);np.random.shuffle(self.data7)
+        # np.random.seed(randnum);np.random.shuffle(self.data8)
+        # np.random.seed(randnum);np.random.shuffle(self.data9)
+        # np.random.seed(randnum);np.random.shuffle(self.data10)
+
 
 
         self.data1 = torch.from_numpy(self.data1)
         self.data2 = torch.from_numpy(self.data2)
-        self.data3 = torch.from_numpy(self.data3)
-        self.data4 = torch.from_numpy(self.data4)
-        self.data5 = torch.from_numpy(self.data5)
-        self.data6 = torch.from_numpy(self.data6)
+        # self.data3 = torch.from_numpy(self.data3)
+        # self.data4 = torch.from_numpy(self.data4)
+        # self.data5 = torch.from_numpy(self.data5)
+        # self.data6 = torch.from_numpy(self.data6)
+        # self.data7 = torch.from_numpy(self.data7)
+        # self.data8 = torch.from_numpy(self.data8)
+        # self.data9 = torch.from_numpy(self.data9)
+        # self.data10= torch.from_numpy(self.data10)
 
-        self.clean_data=torch.cat((self.data4,self.data5,self.data6), 0)
-        self.noisy_data=torch.cat((self.data1, self.data2, self.data3), 0)
+        # self.clean_data=torch.cat((self.data6, self.data7, self.data8,self.data9, self.data10), 0)
+        # self.noisy_data=torch.cat((self.data1, self.data2, self.data3,self.data4, self.data5), 0)
+        self.clean_data = self.data2
+        self.noisy_data = self.data1
 
         numBatch = int(self.clean_data.shape[0] / args.batch_size)
         print(numBatch)
@@ -65,8 +73,8 @@ class Data_loader():
         return numBatch
 
     def load_test_data(self,args):
-        self.test_data1 = load_3D_images(glob.glob('./datasets/test/{}/*.tif'.format(args.test_labels)))
-        self.test_data2 = load_3D_images(glob.glob('./datasets/test/{}/*.tif'.format(args.test_input)))
+        self.test_data1 = load_3D_images(glob.glob('./datasets/{}/*.tif'.format(args.test_labels)))
+        self.test_data2 = load_3D_images(glob.glob('./datasets/{}/*.tif'.format(args.test_input)))
 
     def set_data(self,batch_id,batch_size):
 
@@ -77,55 +85,63 @@ class Data_loader():
 
     def set_test_data(self):
         return {
-            'test_data1': self.test_data1, 'test_data2': self.test_data2,
-            'eval_data1': self.eval_data1, 'eval_data2': self.eval_data2
+            'test_data1': self.test_data1, 'test_data2': self.test_data2
         }
+
 
     def generate_3D_patches(self, filepath):
         # calculate the number of patches
-        im = Image.open(filepath[0])
+
+        img_example = cv2.imread(filepath[0])  # ndarray
+        (h, w, c) = img_example.shape
         count = 0
+        inputs = [None, None]
+
         for s in range(len(scales)):
-            newsize = (int(im.size[0] * scales[s]), int(im.size[1] * scales[s]))
-            im_resized = im.resize(newsize)
-            im_h, im_w = im_resized.size
-            for x in range(0, im_h - PAT_SIZE, STRIDE):
-                for y in range(0, im_w - PAT_SIZE, STRIDE):
+            new_h = int(h * scales[s])
+            new_w = int(w * scales[s])
+            new_c = c
+            new_size = (new_h, new_w, new_c)
+
+
+
+            for x in range(0, new_h - PAT_SIZE, STRIDE):
+                for y in range(0, new_w - PAT_SIZE, STRIDE):
                     count += 1
-        print(count)
+
         if count % BAT_SIZE != 0:
             numpats = (count // BAT_SIZE + 1) * BAT_SIZE
         else:
             numpats = count
-        print('total patches = %d,batch_size = %d,total batches = %d' % (numpats, BAT_SIZE, numpats / BAT_SIZE))
-
-        # generate 3D matrixs
-        input_3Ds = [None]  # , None, None, None
-        for s in range(len(scales)):
-            input_3Ds[s] = np.zeros((len(filepath), int(im.size[0] * scales[s]), int(im.size[1] * scales[s])),
-                                    dtype='uint16')  # 1024*1024*11
-            for i in range(len(filepath)):
-                im = Image.open(filepath[i])
-                newsize = (int(im.size[0] * scales[s]), int(im.size[1] * scales[s]))
-                im_resized = im.resize(newsize)
-                im_array = np.array(im_resized, dtype='uint16')
-                input_3Ds[s][i:i + 1, :, :] = im_array
-
-        # generate_patches
-        inputs = np.zeros((int(numpats), 1, len(filepath), PAT_SIZE, PAT_SIZE), dtype='uint16')
+        print('count = %d,total patches = %d,batch_size = %d,total batches = %d' % (count,numpats, BAT_SIZE, numpats / BAT_SIZE))
         count = 0
-        for input_3D in input_3Ds:
-            input_3D = np.reshape(input_3D, (1,input_3D.shape[0], input_3D.shape[1], input_3D.shape[2]))
-            for x in range(0, input_3D.shape[2] - PAT_SIZE, STRIDE):
-                for y in range(0, input_3D.shape[2] - PAT_SIZE, STRIDE):
-                    inputs[count, :, :, :, :] = input_3D[:,  :,x:x + PAT_SIZE, y:y + PAT_SIZE]
+        labels_patches = np.zeros((numpats, 16, PAT_SIZE, PAT_SIZE, 3))
+        inputs_patches = np.zeros((numpats, 16, PAT_SIZE, PAT_SIZE, 3))
+        for s in range(len(scales)):
+            new_h = int(h * scales[s])
+            new_w = int(w * scales[s])
+            new_c = c
+            new_size = (new_h, new_w, new_c)
+            # print(new_size)
+            # one folder's images->3D matrixs
+
+            inputs[s] = np.zeros((16, int(h * scales[s]), int(w * scales[s]), c))  # 16*1024*1024*3
+            for i in range(16):
+                inputs_layers =  cv2.imread(filepath[i])
+                inputs_layers_resized =  inputs_layers[0:new_h,0:new_w,0:new_c]
+                inputs[s][i:i + 1, :, :, :] = inputs_layers_resized
+            # generate_patches
+            for i in range(0, new_h - PAT_SIZE, STRIDE):
+                for j in range(0, new_w - PAT_SIZE, STRIDE):
                     count += 1
-        print(count)
-        # pad the batch
-        if count < numpats:
+                    inputs_patches[count, :, :, :, :] = inputs[s][:,i:i + PAT_SIZE,j:j + PAT_SIZE, :]
+
+        if count % BAT_SIZE != 0:
             to_pad = numpats - count
-            c = inputs[:int(to_pad), :, :, :, :]
-            # inputs[count:(count+int(to_pad)), :, :, :,:] = inputs[:int(to_pad), :, :, :,:]
-            inputs[-int(to_pad):, :, :, :, :] = inputs[:int(to_pad), :, :, :, :]
-        print("size of inputs tensor = " + str(inputs.shape))
-        return inputs
+            inputs_patches[-int(to_pad):, :, :, :, :] = inputs_patches[:int(to_pad),:, :, :, :]
+
+        # np.transpose(inputs_patches, (0, 4, 1, 2, 3))
+        # inputs_patches.transpose( (0, 4, 1, 2, 3) )
+        inputs_patches = inputs_patches.transpose(0, 4, 1, 2, 3)
+
+        return inputs_patches
--- model.py
+++ model.py
@@ -6,13 +6,13 @@ import numpy as np
 import cv2
 import os
 from torch.autograd import Variable
-torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.benchmark=True
 class denoiser(object):
     def __init__(self,args,input_c_dim=1,sigma=25, batch_size=64):
         self.input_c_dim = input_c_dim
         # build model
         self.batch_size=batch_size
-        self.device = torch.device("cuda:0,1" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.net = Dncnn().to(self.device)
         self.loss = torch.nn.MSELoss(size_average=False)
         self.adjust_learning_rate(args)
@@ -28,11 +28,8 @@ class denoiser(object):
 
 
     def set_test_input(self,input):
-        self.test_data1 = input['test_data1']
-        self.test_data2 = input['test_data2']
-        self.eval_data1 = input['eval_data1']
-        self.eval_data2 = input['eval_data2']
-
+        self.test_data_labels = input['test_data1']
+        self.test_data_input = input['test_data2']
 
     def forward(self):
         self.desired = self.net(self.noisy_images_all)
@@ -51,37 +48,84 @@ class denoiser(object):
         self.loss_all.backward()
         self.optimizer.step()
 
-    def test(self,arg):
-        print("start testing....")
-        self.test_data1 = self.test_data1.astype(np.float32) / 65535.0
-        self.test_data2 = self.test_data2.astype(np.float32) / 65535.0 #[1,1,7,1024,1024]
-        clean_image = Variable(torch.from_numpy(self.test_data1)).to(self.device)
-        noisy_image = Variable(torch.from_numpy(self.test_data2)).to(self.device)
-        self.load_networks(arg,'latest')
+    def evaluate(self, epoch, arg):
+        # assert test_data value range is 0-255
+        print("[*] Evaluating...")
+        self.eval_data1 = self.eval_data1.astype(np.float32) / 255.0
+        self.eval_data2 = self.eval_data2.astype(np.float32) / 255.0
+        clean_image = Variable(torch.from_numpy(self.eval_data1)).to(self.device)
+        noisy_image = Variable(torch.from_numpy(self.eval_data2)).to(self.device)
         with torch.no_grad():
-
             output_clean_image = self.net(noisy_image)
-            clean_image = clean_image.cpu().numpy()   # clean image is label
+            # output = output.permute(0,2,3,4,1)
+            # output_clean_image = noisy_image- output  # that is desired
+            clean_image = clean_image.cpu().numpy()
             noisy_image = noisy_image.cpu().numpy()
             output_clean_image = output_clean_image.cpu().numpy()
-            groundtruth = np.clip(65535 * self.test_data1, 0, 65535).astype('uint16')
-            noisyimage = np.clip(65535 * noisy_image, 0, 65535).astype('uint16')
-            outputimage = np.clip(65535 * output_clean_image, 0, 65535).astype('uint16')
+            groundtruth = np.clip(255 * self.eval_data1, 0, 255).astype('uint16')  # 查看数据类型
+            noisyimage = np.clip(255 * noisy_image, 0, 255).astype('uint16')
+            outputimage = np.clip(255 * output_clean_image, 0, 255).astype('uint16')
+            # 判断x对象是否为空对象，如果都为空、0、false，则返回false，如果不都为空、0、false，则返回true
             if not clean_image.any():
                 clean_image = groundtruth
-            ground_truth = np.reshape(groundtruth, (groundtruth.shape[2], groundtruth.shape[3], groundtruth.shape[4]))  # (7,1024,1024)
-            noisy_image = np.reshape(noisyimage, (noisyimage.shape[2], noisyimage.shape[3], noisyimage.shape[4]))
-            output_clean_image = np.reshape(outputimage, (outputimage.shape[2], outputimage.shape[3], outputimage.shape[4]))
-
+            ground_truth = np.reshape(groundtruth, (
+            groundtruth.shape[2], groundtruth.shape[3], groundtruth.shape[4], 3))  # (16,1024,1024,3)
+            noisy_image = np.reshape(noisyimage, (noisyimage.shape[2], noisyimage.shape[3], noisyimage.shape[4], 3))
+            output_clean_image = np.reshape(outputimage,
+                                            (outputimage.shape[2], outputimage.shape[3], outputimage.shape[4], 3))
             ground_truth_layers = [None] * ground_truth.shape[0]
             noisy_image_layers = [None] * ground_truth.shape[0]
             output_clean_image_layers = [None] * ground_truth.shape[0]
             cat_image_layers = [None] * ground_truth.shape[0]
             for i in range(ground_truth.shape[0]):
-                ground_truth_layers[i] = np.reshape(ground_truth[i:i + 1, :, :],
-                                                    (ground_truth.shape[1], ground_truth.shape[2]))
-                noisy_image_layers[i] = np.reshape(noisy_image[i:i + 1, :, :], (noisy_image.shape[1], noisy_image.shape[2]))
-                output_clean_image_layers[i] = np.reshape(output_clean_image[i:i + 1, :, :], (output_clean_image.shape[1], output_clean_image.shape[2]))
+                ground_truth_layers[i] = np.reshape(ground_truth[i:i + 1, :, :,:],
+                                                    (ground_truth.shape[1], ground_truth.shape[2],3))
+                noisy_image_layers[i] = np.reshape(noisy_image[i:i + 1, :, :,:], (noisy_image.shape[1], noisy_image.shape[2],3))
+                output_clean_image_layers[i] = np.reshape(output_clean_image[i:i + 1, :, :,:], (output_clean_image.shape[1], output_clean_image.shape[2],3))
+                cat_image_layers[i] = np.concatenate([ground_truth_layers[i], noisy_image_layers[i], output_clean_image_layers[i]],
+                                                     axis=1)
+                cv2.imwrite(os.path.join(arg.sample_dir, 'show_layer','%d.tif') % (i), cat_image_layers[i])
+                cv2.imwrite(os.path.join(arg.sample_dir, 'label_layer','%d.tif') % (i), ground_truth_layers[i])
+                cv2.imwrite(os.path.join(arg.sample_dir, 'input_layer','%d.tif') % (i), noisy_image_layers[i])
+                cv2.imwrite(os.path.join(arg.sample_dir, 'denoised_layer','%d.tif') % (i), output_clean_image_layers[i])
+                cv2.imwrite(os.path.join(arg.sample_dir, 'denoised_labels_layer','%d.tif') % (i),
+                            (output_clean_image_layers[i].astype('int16') - ground_truth_layers[i].astype('int16')))
+
+    def test(self,arg):
+        print("start testing....")
+        self.test_data_labels = self.test_data_labels.astype(np.float32) / 255.0 #[1,1,7,2048,2048]
+        self.test_data_input = self.test_data_input.astype(np.float32) / 255.0
+
+        clean_image = Variable(torch.from_numpy(self.test_data_labels)).to(self.device)
+        noisy_image = Variable(torch.from_numpy(self.test_data_input)).to(self.device)  # 1,3,16,1024,1024
+        self.load_networks(arg,'1')
+        with torch.no_grad():
+            output_clean_image = self.net(noisy_image)
+            # output = output.permute(0, 2, 3, 4, 1)
+            # output_clean_image = noisy_image - output  # that is desired
+            clean_image = clean_image.cpu().numpy()   # clean image is label
+            noisy_image = noisy_image.cpu().numpy()
+            output_clean_image = output_clean_image.cpu().numpy()
+            groundtruth = np.clip(255 * self.test_data_labels, 0, 255).astype('uint16')
+            noisyimage = np.clip(255 * noisy_image, 0, 255).astype('uint16')
+            outputimage = np.clip(255 * output_clean_image, 0, 255).astype('uint16')
+
+            if not clean_image.any():
+                clean_image = groundtruth
+            ground_truth = np.reshape(groundtruth, (groundtruth.shape[2], groundtruth.shape[3], groundtruth.shape[4],3))  # (16,1024,1024,3)
+            noisy_image = np.reshape(noisyimage, (noisyimage.shape[2], noisyimage.shape[3], noisyimage.shape[4],3))
+            output_clean_image = np.reshape(outputimage, (outputimage.shape[2], outputimage.shape[3], outputimage.shape[4],3))
+
+            ground_truth_layers = [None] * ground_truth.shape[0]
+            noisy_image_layers = [None] * ground_truth.shape[0]
+            output_clean_image_layers = [None] * ground_truth.shape[0]
+            cat_image_layers = [None] * ground_truth.shape[0]
+
+            for i in range(ground_truth.shape[0]):
+                ground_truth_layers[i] = np.reshape(ground_truth[i:i + 1, :, :,:],
+                                                    (ground_truth.shape[1], ground_truth.shape[2],3))
+                noisy_image_layers[i] = np.reshape(noisy_image[i:i + 1, :, :,:], (noisy_image.shape[1], noisy_image.shape[2],3))
+                output_clean_image_layers[i] = np.reshape(output_clean_image[i:i + 1, :, :,:], (output_clean_image.shape[1], output_clean_image.shape[2],3))
                 cat_image_layers[i] = np.concatenate([ground_truth_layers[i], noisy_image_layers[i], output_clean_image_layers[i]],
                                                      axis=1)
                 cv2.imwrite(os.path.join(arg.sample_dir, 'show_layer','%d.tif') % (i), cat_image_layers[i])
@@ -91,12 +135,14 @@ class denoiser(object):
                 cv2.imwrite(os.path.join(arg.sample_dir, 'denoised_labels_layer','%d.tif') % (i),
                             (output_clean_image_layers[i].astype('int16') - ground_truth_layers[i].astype('int16')))
                 mse = ((ground_truth_layers[i].astype(np.float) - output_clean_image_layers[i].astype(np.float)) ** 2).mean()
-                psnr = 10 * np.log10(65535 ** 2 / mse)
-                print('psnr for layer%d is %f' % (i, psnr))
-                # mse = ((noisy_image_layers[i].astype(np.float) - output_clean_image_layers[i].astype(np.float)) ** 2).mean()
+                # psnr = 10 * np.log10(65535 ** 2 / mse)
+                # print('psnr for layer%d is %f' % (i, psnr))
+                mse = ((noisy_image_layers[i].astype(np.float) - output_clean_image_layers[i].astype(np.float)) ** 2).mean()
                 # psnr2 = 10 * np.log10(65535 ** 2 / mse)
-                # 
+                #
                 # print('psnr for layer%d is %f' % (i, psnr2))
+
+
 
     def save_networks(self,args,epoch):
 
@@ -123,13 +169,14 @@ class denoiser(object):
         load_path = os.path.join(args.ckpt_dir, load_filename)
         print('loading the model from %s' % load_path)
         # self.net.eval()
-        self.net.load_state_dict(torch.load(load_path))
-
         # print(self.net)
         # for param in self.net.parameters():
         #     print(type(param.data), param.size())
         #     print(list(param.data))
-        #
+        self.net.load_state_dict(torch.load(load_path))
+
+
+
         # print(self.net.state_dict().keys())
         # # 参数的keys
         #
